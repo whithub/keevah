9420fdc2db47285938e6c988bb44c9b46b151783
Whitney Hiemstra <whitcolorado@gmail.com>
2015-09-10 08:57:55 -0600

    Oh you enormous seed file, you are the death of me

--- db/enormous_seed.rb
+++ db/enormous_seed.rb
@@ -5,10 +5,10 @@ module EnormousSeed
   class Seed
     def run
       create_known_users
+      create_lenders(200000)
+      create_borrowers(30000)
       create_categories
-      25.times { create_lenders }
-      6.times  { create_borrowers }
-      50.times { create_loan_requests }
+      create_loan_requests(500000)
       create_orders(50000)
     end
 
@@ -41,28 +41,28 @@ module EnormousSeed
       end
     end
 
-    def create_lenders
-      User.populate(8000) do |user|
+    def create_lenders(quantity)
+      User.populate(quantity) do |user|
         user.name = Faker::Name.name
         user.email = Faker::Internet.email
         user.password_digest = "$2a$10$3SBt75c.BIcW/TO6H58FfOgGpKm47GLTWrb/416u9uS6xSAJS7PL6"
         user.role = 0
       end
-      puts "created 8000 lenders"
+      puts "created #{quantity} lenders"
     end
 
-    def create_borrowers
-      User.populate(5000) do |user|
+    def create_borrowers(quantity)
+      User.populate(quantity) do |user|
         user.name = Faker::Name.name
         user.email = Faker::Internet.email
         user.password_digest = "$2a$10$3SBt75c.BIcW/TO6H58FfOgGpKm47GLTWrb/416u9uS6xSAJS7PL6"
         user.role = 1
+        puts "created #{quantity} orders"
       end
-      puts "created 5000 borrowers"
     end
 
-    def create_loan_requests
-      LoanRequest.populate(10000) do |loan_request|
+    def create_loan_requests(quantity)
+      LoanRequest.populate(quantity) do |loan_request|
         loan_request.user_id = borrowers.sample.id
         loan_request.title = Faker::Commerce.product_name
         loan_request.description = Faker::Company.catch_phrase
@@ -77,14 +77,11 @@ module EnormousSeed
           lrcategory.loan_request_id = loan_request.id
           lrcategory.category_id = Category.all.sample.id
         end
-        puts "There are now #{LoanRequest.count} requests"
-
+        puts "There are now #{quantity} requests"
       end
-      # puts "created 10000 loan_requests"
     end
 
     def create_orders(quantity)
-      # loan_requests = LoanRequest.take(50000)
       possible_donations = %w(25, 50, 75, 100, 125, 150, 175, 200)
       quantity.times do
         donate = possible_donations.sample
@@ -95,7 +92,7 @@ module EnormousSeed
                             user_id: lender.id)
         order.update_contributed(lender)
       end
-      puts "created 50000 orders"
+      puts "created #{quantity} orders"
     end
   end
 end
