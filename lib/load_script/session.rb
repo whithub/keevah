2b8ccbd771dfcf07cfd1c9a94a7e45688ef5a3c0
Whitney Hiemstra <whitcolorado@gmail.com>
2015-09-13 12:17:21 -0600

    Lender able to make loan

--- lib/load_script/session.rb
+++ lib/load_script/session.rb
@@ -147,7 +147,11 @@ module LoadScript
     end
 
     def lender_makes_loan
-      skip
+      sign_up_as_lender
+      view_individual_LR
+      session.click_on("Contribute $25")
+      session.click_on("Basket")
+      session.click_on("Transfer Funds")
     end
 
     def browse_loan_requests
