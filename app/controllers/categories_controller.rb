f5ddff17522b21fa86a14ddfff9c5a03cce4f058
Whitney Hiemstra <whitcolorado@gmail.com>
2015-09-16 00:07:50 -0600

    dont load into memory

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @categories = Category.all
-    @category = Category.includes(:loan_requests).find(params[:id])
+    @category = Category.find(params[:id])
     @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 12)
   end
 
